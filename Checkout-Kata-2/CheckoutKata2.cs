229e5205b23a0a7d9003fff9b6801e8fabd93866
Chris Watson <chris.watson@7digital.com>
2013-02-11 17:20:40 +0000

    Implementation for a, aa and aaa with discount

--- Checkout-Kata-2/CheckoutKata2.cs
+++ Checkout-Kata-2/CheckoutKata2.cs
@@ -3,12 +3,43 @@
 namespace Checkout_Kata_2
 {
 	[TestFixture]
-    public class CheckoutKata2
-    {
+	public class CheckoutKata2
+	{
 		[Test]
-		public void test()
+		public void Checkout_total_should_be_zero()
 		{
-			
+			var checkout = new Checkout();
+			Assert.That(checkout.Total, Is.EqualTo(0));
 		}
-    }
+
+		[TestCase(1, 50)]
+		[TestCase(2, 100)]
+		[TestCase(3, 130)]
+		public void Checkout_scan_number_of_item_a_should_return_the_expected_total(int numberOfAs, int expectedTotal)
+		{
+			var checkout = new Checkout();
+			var basket = "";
+			for (var i = 0; i < numberOfAs; i++)
+				basket += 'a';
+			checkout.Scan(basket);
+			Assert.That(checkout.Total, Is.EqualTo(expectedTotal));
+		}
+	}
+
+	public class Checkout
+	{
+		public int Total { get; private set; }
+
+		public void Scan(string basket)
+		{
+			var items = basket.ToCharArray();
+			var numAs = 0;
+			foreach (var item in items)
+			{
+				++numAs;
+				Total += 50;
+			}
+			if (numAs == 3) Total -= 20;
+		}
+	}
 }
